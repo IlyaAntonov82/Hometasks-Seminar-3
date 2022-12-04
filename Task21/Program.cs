d3808ecc21e73f4ff89e7f8575b83de2f0d0698c
«IlyaAntonov» <antonovilya82@gmail.com>
2022-12-04 08:59:22 +0500

    исправлена задача 21

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -6,4 +6,19 @@
 
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Hello, World!");
+Console.Write("Введите X координату точки A: ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y координату точки A: ");
+int Y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z координату точки A: ");
+int Z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите X координату точки B: ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y координату точки B: ");
+int Y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z координату точки B: ");                                                         
+int Z2 = Convert.ToInt32(Console.ReadLine());                                                           
+
+double length = Math.Round((Math.Sqrt(Math.Pow((X2-X1),2) + Math.Pow((Y2 - Y1),2) + Math.Pow((Z2 - Z1),2))),2); 
+Console.WriteLine("Расстояние между точками A и B в 3D пространстве: " + length);
