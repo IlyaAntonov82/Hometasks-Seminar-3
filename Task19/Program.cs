7a55d04c8f8be60e308d8be49698d07b420361e2
«IlyaAntonov» <antonovilya82@gmail.com>
2022-12-04 08:50:57 +0500

    исправлена задача 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -8,4 +8,18 @@
 
 // 23432 -> да
 
-Console.WriteLine("Hello, World!");
+Console.Write("Введите пятизначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num>9999 && num<=99999)
+{
+  int numReverse = 0;
+  int numCopy = num;
+  while (num > 0)
+  {
+    numReverse = numReverse * 10 + (num %10);
+    num = num / 10;
+  };
+  if (numReverse == numCopy) Console.Write("Число является палиндромом");
+  else Console.Write("Число не является палиндромом");
+} 
+else Console.Write("Введено неправильное число");
